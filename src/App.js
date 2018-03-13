3eaa7a57bf233760a0f81300a37dfba851ea00e7
Nik Lampe <nik.lampe@gmail.com>
2018-03-13 10:10:06 +0100

    netlify config

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          NODE_ENV: {process.env.NODE_ENV}<br/>
+          API URL: {process.env.REACT_APP_API_URL}
         </p>
       </div>
     );
